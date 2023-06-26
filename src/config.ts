22847ef3b61e5e89415c3b8a65c4f6def47d8e93
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-06-25 21:41:30 -0500

    cambios PORT

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,4 @@ export const DB_USER = process.env.DB_USER || 'postgres';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '0946';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_DATABASE = process.env.DB_DATABASE || 'Hotel';
-export const DB_PORT = 5432;
+export const DB_PORT = parseInt(process.env.DB_PORT || '5432', 10);
