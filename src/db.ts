78d4805f8959f5c9fb42e98581a584cf181ede1f
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-07-19 13:19:11 -0500

    cambios bd

--- src/db.ts
+++ src/db.ts
@@ -2,22 +2,22 @@ import { DataSource } from 'typeorm';
 import Entities from './entities';
 import { DB_DATABASE,DB_HOST,DB_PASSWORD,DB_PORT,DB_USER, DATABASE_URL } from './config';
 
-export default new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || DB_HOST,
-  username: process.env.DB_USER || DB_USER,
-  password: process.env.DB_PASSWORD || DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : DB_PORT,
-  database: process.env.DB_DATABASE || DB_DATABASE,
-  entities: Entities,
-  synchronize: true,
-  ssl: true
-});
-
 // export default new DataSource({
 //   type: 'postgres',
-//   url: DATABASE_URL,
+//   host: process.env.DB_HOST || DB_HOST,
+//   username: process.env.DB_USER || DB_USER,
+//   password: process.env.DB_PASSWORD || DB_PASSWORD,
+//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : DB_PORT,
+//   database: process.env.DB_DATABASE || DB_DATABASE,
 //   entities: Entities,
 //   synchronize: true,
 //   ssl: true
 // });
+
+export default new DataSource({
+  type: 'postgres',
+  url: DATABASE_URL,
+  entities: Entities,
+  synchronize: true,
+  ssl: true
+});
