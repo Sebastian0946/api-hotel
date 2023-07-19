e47b18a7c37fdf4ef02860684075a65f8a155251
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-07-19 13:08:20 -0500

    cambios conexion y variables de entorno

--- dist/db.js
+++ dist/db.js
@@ -8,18 +8,19 @@ const entities_1 = __importDefault(require("./entities"));
 const config_1 = require("./config");
 exports.default = new typeorm_1.DataSource({
     type: 'postgres',
-    url: config_1.DATABASE_URL,
+    host: process.env.DB_HOST || config_1.DB_HOST,
+    username: process.env.DB_USER || config_1.DB_USER,
+    password: process.env.DB_PASSWORD || config_1.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : config_1.DB_PORT,
+    database: process.env.DB_DATABASE || config_1.DB_DATABASE,
     entities: entities_1.default,
     synchronize: true,
     ssl: true
 });
+
 // export default new DataSource({
 //   type: 'postgres',
-//   host: process.env.DB_HOST || DB_HOST,
-//   username: process.env.DB_USER || DB_USER,
-//   password: process.env.DB_PASSWORD || DB_PASSWORD,
-//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : DB_PORT,
-//   database: process.env.DB_DATABASE || DB_DATABASE,
+//   url: DATABASE_URL,
 //   entities: Entities,
 //   synchronize: true,
 //   ssl: true
