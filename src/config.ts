44981d4933ee27cec2b83d47dda49393c6e488c2
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-07-19 12:35:35 -0500

    cambias conexion

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,5 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || '0946';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_DATABASE = process.env.DB_DATABASE || 'Hotel';
 export const DB_PORT = parseInt(process.env.DB_PORT || '5432', 10);
+
+export const DATABASE_URL = 'postgres://sebas:gQzZjtrLyk5h0mPLD5KWUdxtElct2Jah@dpg-cis1d4p8g3n42okr38pg-a.oregon-postgres.render.com/hotel_ode6'
