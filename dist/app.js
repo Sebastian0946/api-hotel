98c10223fd9f8162110b80d5f1b2f3842a09e369
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-10-13 15:08:41 -0500

    cambios respuesta api

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser")); // Importar body-parser
 const routes_1 = __importDefault(require("./routes"));
 const errorHandler_1 = require("./errorHandler");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)('dev'));
+app.use(body_parser_1.default.json({ limit: '50mb' }));
+app.use(body_parser_1.default.urlencoded({ limit: '50mb', extended: true }));
 app.use(errorHandler_1.errorHandler);
 app.use(express_1.default.json());
 app.use("/api", routes_1.default);
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser'; // Importar body-parser
 
 import routes from './routes';
 import { errorHandler } from './errorHandler';
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(errorHandler);
-app.use(express.json())
 
-app.use("/api", routes)
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(errorHandler);
+app.use(express.json());
+app.use("/api", routes);
 
 app.use((req, res, next) => {
     res.status(404).json({
@@ -23,4 +27,4 @@ app.use((req, res, next) => {
     });
 });
 
-export default app
+export default app;
