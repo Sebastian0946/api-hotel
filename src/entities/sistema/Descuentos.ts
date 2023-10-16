5b9f155643699ca03bd57eacf5c73e88d3d04f6e
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-10-16 08:59:53 -0500

    Cambios tabla descuento

--- src/entities/sistema/Descuentos.ts
+++ src/entities/sistema/Descuentos.ts
@@ -1,16 +1,12 @@
 import { Column, Entity, OneToMany } from "typeorm";
 import { ModelEntity } from "../ModelEntity";
 import { ReservaHabitaciones } from "./ReservaHabitaciones";
-import { Huespedes } from "./Huespedes";
 
 @Entity({schema: ''})
 export class Descuentos extends ModelEntity{
 
     @Column({name: 'codigo',unique: true, length: 25, nullable: false})
     Codigo: String;
-    
-    @Column({name: 'valorNeto', type: 'double precision', nullable: false})
-    ValorNeto: number;
 
     @Column({name: 'porcentajeDescuento', type: 'double precision', nullable: false})
     PorcentajeDescuento: number;
