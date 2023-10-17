10e35448e1b7806928d79166f9c216c8e5fb43ea
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-10-17 14:30:11 -0500

    Cambios check-in habitacion

--- src/entities/sistema/ConsumoHabitaciones.ts
+++ src/entities/sistema/ConsumoHabitaciones.ts
@@ -13,9 +13,6 @@ export class ConsumoHabitaciones extends ModelEntity {
     @Column({ name: 'codigo', unique: true, length: 25, nullable: false })
     Codigo: String;
 
-    @Column({ name: 'cantidad', nullable: false })
-    Cantidad: String;
-
     @OneToOne(() => EstadoFacturas, (estadoFactura) => estadoFactura.ConsumoHabitacionesId)
     EstadoFacturaId: EstadoFacturas;
 }
