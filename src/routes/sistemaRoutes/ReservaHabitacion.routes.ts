6980e4582d1f1052ca1e83b797f8403ff59d77ed
Sebastian Cabrera <114190080+Sebastian0946@users.noreply.github.com>
2023-10-21 10:11:48 -0500

    Cambio ruta buscar codigo reserva

--- src/routes/sistemaRoutes/ReservaHabitacion.routes.ts
+++ src/routes/sistemaRoutes/ReservaHabitacion.routes.ts
@@ -16,7 +16,7 @@ router.get('/sistema/reservaHabitacion', controller.list.bind(controller));
 
 router.get('/sistema/reservaHabitacion/:id', controller.get.bind(controller));
 
-router.get('/sistema/reservaHabitacion/:Codigo', controller.getCodigo.bind(controller));
+router.get('/sistema/reservaHabitacion/buscarCodigo/:Codigo', controller.getCodigo.bind(controller));
 
 router.put('/sistema/reservaHabitacion/:id', controller.update.bind(controller));
 
